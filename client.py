3d7f1a071890d8742c8ed8e19fd9c43e8ee98f57
tejas303525 <68140019+tejas303525@users.noreply.github.com>
2021-11-20 13:27:42 +0400

    Update client.py

--- client.py
+++ client.py
@@ -4,9 +4,15 @@ import socket
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 s.connect(('localhost',4444))
-ip=input("Enter IP:")
-
-s.send(ip.encode())
-mac=s.recv(1024)
-mac=mac.decode()
-print("mac address:", mac)
+connected = True
+while connected:
+    ip=input("Enter IP:")
+    s.send(ip.encode())
+    mac=s.recv(1024)
+    mac=mac.decode()
+    print("mac address:", mac)
+    ips=input('Do you want to continue: y or n :')
+    if ips =='n':
+        s.send('!disconnect').encode()
+    else:
+        continue 
